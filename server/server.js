1bffc0134830c82e97943993d38509be4de0f283
iliashka <69167918+iliashka@users.noreply.github.com>
2021-03-15 22:52:15 +0300

    make it better

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 const User = require('./models/userModel');
 const routes = require('./routes/route');
+const cors = require('cors');
+const path = require('path')
 
 
 require('dotenv').config({
@@ -13,6 +15,8 @@ require('dotenv').config({
 
 const app = express();
 
+app.use(cors())
+app.use(express.static(path.join(__dirname, './build')))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}))
 
@@ -23,6 +27,9 @@ mongoose
     });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build'))
+})
 
 app.use(async (req, res, next) => {
     if (req.headers['x-access-token']) {
