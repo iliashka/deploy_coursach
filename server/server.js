6404c8da47fd421ed37e4b39792455b48f5e28b0
iliashka <69167918+iliashka@users.noreply.github.com>
2021-03-16 10:24:28 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -44,10 +44,10 @@ app.use(async (req, res, next) => {
 app.use('/', routes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'cd ./client', 'build')))
+    app.use('/client', express.static(path.join(__dirname, 'client', 'build')))
 
     app.get('*', ((req, res) => {
-        res.sendFile(path.resolve(__dirname, 'cd ./client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     }))
 }
 
