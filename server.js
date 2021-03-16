3358824f46e3db093fcb64136ad8539a6e7591ba
iliashka <69167918+iliashka@users.noreply.github.com>
2021-03-16 12:52:54 +0300

    deploy

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(async (req, res, next) => {
 app.use('/api/', routes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('./client/build'))
 
     app.get('*', ((req, res) =>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
